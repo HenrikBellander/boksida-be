d73858b20b32698e7fb0f3290ced5f1c1a65c15e
David Abdulkarim <david.abdulkarim@hotmail.com>
2025-03-22 20:01:19 +0100

    Added CORS to enable communication between backend and React frontend, and added url_prefix='/api' to existing Blueprint for better structure.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from .routes import books
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(books)
+    CORS(app)
+    app.register_blueprint(books) 
     return app
 
